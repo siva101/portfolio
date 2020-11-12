979f4d9223d9b25ef561c068ad9c610b0df58725
Siva <ajithsiva100@gmail.com>
2020-11-13 00:32:37 +0530

    final

--- blog/models.py
+++ blog/models.py
@@ -6,5 +6,10 @@ class Blog(models.Model):
     pub_date=models.DateTimeField()
     body=models.TextField()
 
-
+    def __str__(self):
+        return self.title
+    def summary(self):
+        return self.body[:50]
+    def pubdate(self):
+        return self.pub_date.strftime('%b %e %Y')
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Blog
+
+def blog(request):
+    blogs=Blog.objects
+    return render(request,'blog/blog.html',{'blogs':blogs})
+
+def details(request,blog_id):
+    blogdetail=get_object_or_404(Blog,pk=blog_id)
+    return render(request,'blog/detail.html',{'blog':blogdetail})
 
 # Create your views here.
